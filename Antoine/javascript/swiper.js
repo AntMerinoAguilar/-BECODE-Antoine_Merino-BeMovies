3abc32261df450d867252984b420f0ec8dcdcf7b
Timmy <timmy2O.10@hotmail.com>
2024-11-13 18:38:40 +0100

    api part done

--- Antoine/javascript/swiper.js
+++ Antoine/javascript/swiper.js
@@ -1,13 +1,13 @@
 // JS pour les swiper
 
-const swiper = new Swiper(".swiper-container", {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 4,
-    spaceBetween: 18,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    mousewheel: true,
-});
+// const swiper = new Swiper(".swiper-container", {
+//     direction: 'horizontal',
+//     loop: true,
+//     slidesPerView: 4,
+//     spaceBetween: 18,
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     mousewheel: true,
+// });
